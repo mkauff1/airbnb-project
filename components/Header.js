b6409c1a79edce25b82602a316033a4e507a4d36
Matthew Kauff <matthew.r.kauff@gmail.com>
2022-06-07 13:26:24 -0400

    add finished banner with sm, md, and lg cards for destinations

--- components/Header.js
+++ components/Header.js
@@ -25,9 +25,13 @@ function Header() {
             </div>
 
             {/* Right*/}
-            <div className={"flex"}>
-                <p>Become a host</p>
-                <GlobeAltIcon className={"h-6"} />
+            <div className={"flex items-center space-x-4 justify-end text-gray-500"}>
+                <p className={"hidden md:inline cursor-pointer"}>Become a host</p>
+                <GlobeAltIcon className={"h-6 cursor-pointer"} />
+                <div className={"flex items-center space-x-2 border-2 p-2 rounded-full"}>
+                    <MenuIcon className={"h-6"}/>
+                    <UserCircleIcon className={"h-6"} />
+                </div>
             </div>
 
         </header>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 module.exports = {
-  content: [
+  mode:'jit',
+  purge: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: false,
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
