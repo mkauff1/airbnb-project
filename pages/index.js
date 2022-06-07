2fc0e4b1f17a6e237d38f6a7d6323917768298b8
Matthew Kauff <matthew.r.kauff@gmail.com>
2022-06-07 15:23:54 -0400

    add footer to the webpage to finish up the frontend

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Banner from "../components/Banner"
 import SmallCard from "../components/SmallCard"
 import MediumCard from "../components/MediumCard";
 import LargeCard from "../components/LargeCard";
+import Footer from "../components/Footer";
 
 export default function Home({exploreData, cardsData}) {
   return (
@@ -41,6 +42,10 @@ export default function Home({exploreData, cardsData}) {
             <section>
                 <LargeCard img={'https://links.papareact.com/4cj'} title={"The Greatest outdoors"}
                             description={"Wishlists created by Airbnb"} buttonText={"Get Inspired"}/>
+            </section>
+
+            <section>
+                <Footer />
             </section>
         </main>
     </div>
